f900a99b57c31a4ea813a1648d396be68a3a1b6a
Chao Zhang <public.zhang.chao@gmail.com>
2012-09-27 14:53:36 -0500

    Find user example code added to main function

--- ARChat/BodyScanner/BodyScanner.cpp
+++ ARChat/BodyScanner/BodyScanner.cpp
@@ -3,9 +3,98 @@
 
 #include "stdafx.h"
 
+#define SAMPLE_XML_PATH "../../../Data/SamplesConfig.xml"
 
-int _tmain(int argc, _TCHAR* argv[])
+#define CHECK_RC(nRetVal, what)										\
+	if (nRetVal != XN_STATUS_OK)									\
+	{																\
+		printf("%s failed: %s\n", what, xnGetStatusString(nRetVal));\
+		return nRetVal;												\
+	}
+
+#define GL_WIN_SIZE_X 720
+#define GL_WIN_SIZE_Y 480
+
+//---------------------------------------------------------------------------
+// Globals
+//---------------------------------------------------------------------------
+xn::Context g_context;
+xn::ScriptNode g_scriptNode;
+xn::DepthGenerator g_depthGenerator;
+xn::UserGenerator g_userGenerator;
+xn::Player g_player;
+
+XnBool g_bNeedPose = FALSE;
+XnChar g_strPose[20] = "";
+XnBool g_bDrawBackground = TRUE;
+XnBool g_bDrawPixels = TRUE;
+XnBool g_bDrawSkeleton = TRUE;
+XnBool g_bPrintID = TRUE;
+XnBool g_bPrintState = TRUE;
+
+XnBool g_bPause = false;
+XnBool g_bRecord = false;
+
+XnBool g_bQuit = false;
+
+
+
+int main(int argc, char **argv)
 {
-	return 0;
-}
+	XnStatus nRetVal = XN_STATUS_OK;
 
+	xn::EnumerationErrors errors;
+	nRetVal = g_Context.InitFromXmlFile(SAMPLE_XML_PATH, g_scriptNode, &errors);
+
+	if (nRetVal == XN_STATUS_NO_NODE_PRESENT)
+	{
+		XnChar strError[1024];
+		errors.ToString(strError, 1024);
+		printf("%s\n", strError);
+		return (nRetVal);
+	}
+	else if (nRetVal != XN_STATUS_OK)
+	{
+		printf("Open failed: %s\n", xnGetStatusString(nRetVal));
+		return (nRetVal);
+	}
+
+	nRetVal = g_Context.FindExistingNode(XN_NODE_TYPE_DEPTH, g_DepthGenerator);
+	CHECK_RC(nRetVal, "Find depth generator");
+
+	nRetVal = g_Context.FindExistingNode(XN_NODE_TYPE_USER, g_UserGenerator);
+	if (nRetVal != XN_STATUS_OK)
+	{
+		nRetVal = g_UserGenerator.Create(g_Context);
+		CHECK_RC(nRetVal, "Find user generator");
+	}
+
+	XnCallbackHandle hUserCallbacks, hCalibrationCallbacks, hPoseCallbacks;
+	if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_SKELETON))
+	{
+		printf("Supplied user generator doesn't support skeleton\n");
+		return 1;
+	}
+	//g_UserGenerator.RegisterUserCallbacks(User_NewUser, User_LostUser, NULL, hUserCallbacks);
+	//g_UserGenerator.GetSkeletonCap().RegisterCalibrationCallbacks(UserCalibration_CalibrationStart, UserCalibration_CalibrationEnd, NULL, hCalibrationCallbacks);
+
+	if (g_UserGenerator.GetSkeletonCap().NeedPoseForCalibration())
+	{
+		g_bNeedPose = TRUE;
+		if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION))
+		{
+			printf("Pose required, but not supported\n");
+			return 1;
+		}
+		//g_UserGenerator.GetPoseDetectionCap().RegisterToPoseCallbacks(UserPose_PoseDetected, NULL, NULL, hPoseCallbacks);
+		g_UserGenerator.GetSkeletonCap().GetCalibrationPose(g_strPose);
+	}
+
+	g_UserGenerator.GetSkeletonCap().SetSkeletonProfile(XN_SKEL_PROFILE_ALL);
+
+	nRetVal = g_Context.StartGeneratingAll();
+	CHECK_RC(nRetVal, "StartGenerating");
+
+	//glInit(&argc, argv);
+	//glutMainLoop();
+}
